dfdc5bd7c4ac8eec99829159c62b887e08a41d9f
Joel Edwards <joeledwards@gmail.com>
2018-04-13 22:42:27 -0600

    feat: updated README and example.js

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # scheduler
 
 A simple, in-memory scheduler implemented in pure JavaScript.
+
+## Usage
+
+```
+const scheduler = require('@buzuli/scheduler')()
+scheduler.after(1000, console.log('a second has elapsed'))
+scheduler.at(Date.now + 500, console.log('half a second has elapsed'))
+```
--- example.js
+++ example.js
@@ -6,3 +6,7 @@ sched.after(1, () => console.log('third'))
 sched.after(2000, () => console.log('fourth'))
 sched.after(500, () => console.log('fifth'))
 sched.after(1000, () => sched.after(20, () => console.log('nested')))
+
+sched.after(2000, () => console.log('two seconds have elapsed'))
+sched.after(1000, () => console.log('a second has elapsed'))
+sched.at(Date.now() + 500, () => console.log('half a second has elapsed'))
